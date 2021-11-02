ec8b7aa523d3dc309976d7527ecbcf1f765b9ecb
leetsh <leetsh@gsitm.com>
2021-11-02 11:08:23 +0900

    spring-boot added.

---
+++ src/main/java/com/github/springcloudaws/SpringCloudAwsApplication.java
@@ -0,0 +1,13 @@
+package com.github.springcloudaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/springcloudaws/SpringCloudAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springcloudaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
